0270169747e60bda109a090a93c0e74caa95cbb2
Manal Ait Dik <maitdik@SMB.SQLI.COM>
2021-03-08 14:16:09 +0100

    add code

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -1,16 +1,54 @@
 package ma.sqli.brute.force;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : El Mahdi Benzekri
  * @since : 3/7/21, dim.
  **/
 public class BruteForceApp {
 
-    public String login(String admin, String password) {
-        return "";
-    }
+	static List<User> users = new ArrayList<User>();
 
-    public void addUser(String admin, String s) {
+	public String login(String admin, String password) {
 
-    }
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getLogin().equals(admin) && users.get(i).getPassword().equals(password)) {
+				users.get(i).setLogin(true);
+
+				return "Welcome " + admin + "!";
+			}
+		}
+
+		return verifyCredential(admin);
+	}
+
+	public String verifyCredential(String admin) {
+
+		User userLogin = new User();
+
+		for (User user : users) {
+			if (user.getLogin().equals(admin)) {
+
+				user.setNbTries(user.getNbTries() + 1);
+				userLogin = user;
+			}
+		}
+
+		if (userLogin.getNbTries() > 2 && userLogin.isLogin() == false) {
+
+			return "Multiple erroneous credentials, your account is locked.";
+		}
+
+		return "User or password are incorrect.";
+
+	}
+
+	public void addUser(String admin, String s) {
+		User user = new User(admin, s);
+		users.add(user);
+
+	}
+
 }
---
+++ src/main/java/ma/sqli/brute/force/User.java
@@ -0,0 +1,45 @@
+package ma.sqli.brute.force;
+
+public class User {
+	
+	private String login;
+	private String password;
+	private int nbTries;
+	private boolean isLogin=false;
+	
+	public boolean isLogin() {
+		return isLogin;
+	}
+	public void setLogin(boolean isLogin) {
+		this.isLogin = isLogin;
+	}
+	public User(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+	}
+	public User() {
+		super();
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getNbTries() {
+		return nbTries;
+	}
+	public void setNbTries(int nbTries) {
+		this.nbTries = nbTries;
+	}
+	
+	
+
+}
